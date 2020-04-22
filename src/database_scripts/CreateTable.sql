67395cedb4d13da440e02957f2afe6d84c69df88
Rafael Marchesin <rmarchesin@gmail.com>
2020-04-22 12:12:08 -0300

    feat: Criado tabela projections

--- src/database_scripts/CreateTable.sql
+++ src/database_scripts/CreateTable.sql
@@ -5,3 +5,11 @@ version INT (1) default 1,
 task VARCHAR(140) NOT NULL,
 done INT(1) default 0
 );
+
+CREATE TABLE todo_list_projections (
+id INT(8) not null AUTO_INCREMENT PRIMARY KEY,
+task_id INT (8) NOT NULL,
+version INT (1) default 1,
+task VARCHAR(140),
+done INT(1) default 0
+);
