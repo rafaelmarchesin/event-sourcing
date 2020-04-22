00cd38a5bef221452a8d46efc5607aaf0933ffc1
Rafael Marchesin <rmarchesin@gmail.com>
2020-04-22 16:39:30 -0300

    feat: Acrescentado TIMESTAMP no banco de dados

--- src/database_scripts/CreateTable.sql
+++ src/database_scripts/CreateTable.sql
@@ -3,7 +3,8 @@ id INT(8) not null AUTO_INCREMENT PRIMARY KEY,
 task_id INT (8) NOT NULL,
 version INT (1) default 1,
 task VARCHAR(140) NOT NULL,
-done INT(1) default 0
+done INT(1) default 0,
+date_time DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE todo_list_projections (
@@ -11,5 +12,6 @@ id INT(8) not null AUTO_INCREMENT PRIMARY KEY,
 task_id INT (8) NOT NULL,
 version INT (1) default 1,
 task VARCHAR(140),
-done INT(1) default 0
+done INT(1) default 0,
+date_time DATETIME DEFAULT CURRENT_TIMESTAMP
 );
